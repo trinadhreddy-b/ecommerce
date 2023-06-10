c5fd1142360002f2a27268c35e1411633c658d04
trinadh reddy bheemireddy <trinadhreddy.b@gmail.com>
2023-06-10 22:54:19 +0530

    slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,55 +1,107 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import { styled } from "styled-components";
+import { sliderItems } from "../data";
+import { useState } from "react";
 
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `;
 const Arrow = styled.div`
-  height: 50px;
   width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: white;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: white;
-  border-radius: 50%;
   position: absolute;
   top: 0;
   bottom: 0;
-  margin: auto;
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
   opacity: 0.5;
   cursor: pointer;
+  z-index: 2;
 `;
 
-const Wrapper=styled.div`
-display: flex;
-width: 100vw;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform:translateX(${props=>(props.slideIndex* -100)}vw);
+ 
 `;
-const ImgContainer=styled.div`
-flex:1
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #${props=>props.bg};
 `;
-const InfoContainer=styled.div`
-flex: 1;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
 `;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+`;
+const Image = styled.img`
+  height: 100%;
+`;
+
 const Slider = () => {
+
+    const [slideIndex, setSlideIndex]=useState(0);
+    const handleClick=(direction)=>{
+
+        if(direction==="left"){
+            setSlideIndex(slideIndex>0 ? slideIndex-1 : 2)
+        }
+        else{
+            setSlideIndex(slideIndex<2 ? slideIndex+1 : 0)
+        }
+        
+    }
   return (
     <Container>
-      <Arrow direction={"left"}>
+      <Arrow direction={"left"} onClick={()=>handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-    <Wrapper>
-        <ImgContainer>
-        </ImgContainer>
-        <InfoContainer>
-
-        </InfoContainer>
-    </Wrapper>
-      <Arrow direction={"right"}>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOP NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction={"right"} onClick={()=>handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://rukminim1.flixcart.com/image/832/832/kqgyhe80/sari/z/j/t/free-greyt-1152-1-kashvi-sarees-unstitched-original-imag4h6mft9kydgv.jpeg?q=70",
+      title: "WOMEN FASHION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://rukminim1.flixcart.com/image/832/832/kr58yvk0/t-shirt/c/n/k/m-r-hf-451-black-london-hills-original-imag4ztguqrkfsec.jpeg?q=70",
+      title: "MENS WEAR",
+      desc: "DON'T COMPROMISE ON STYLE! GET UPTO 60% OFF ON MENS WEAR.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://rukminim1.flixcart.com/image/832/832/xif0q/kids-apparel-combo/q/w/1/5-6-years-l435-lemon-fashion-original-imagjezxurf8ezzg.jpeg?q=70",
+      title: "KIDS FASHION SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF ON KIDS WEAR.",
+      bg: "fbf0f4",
+    },
+  ];
